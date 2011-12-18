275f7a4bf364902d57e91782408ebe77207850f7
Andrei Beliankou <arbox@yandex.ru>
2011-12-18 21:37:01 +0100

    Simulated the german tokenizer. Dummy functionality.

--- lib/treetagger/tagger.rb
+++ lib/treetagger/tagger.rb
@@ -2,5 +2,20 @@
 
 module TreeTagger
   class Tagger
+    def initialize(
+                   lang = :de,
+                   opts = {
+                     :sgml => true,
+                     :token => true,
+                     :lemma => true
+                   }
+                   )
+      @lang = lang
+      @opt = opts
+    end
+    def process(str)
+      %x("echo '#{str}' | #{ENV['TREETAGGERHOME']}/cmd/tree-tagger-german")
+    end
   end # class
 end # module
+
