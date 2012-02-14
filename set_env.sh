cf2d89bd6ece71fbecf84c6e4710194dc2d21c64
Andrei Beliankou <arbox@yandex.ru>
2012-02-14 16:39:00 +0100

    Added a helper file to set environment variables.

---
+++ set_env.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+
+# Export env variables.
+export TREETAGGER_BINARY='/opt/TreeTagger/bin/tree-tagger'
+export TREETAGGER_MODEL='/opt/TreeTagger/lib/german.par'
+export TREETAGGER_LEXICON='/opt/TreeTagger/lib/german-lexicon.txt'
