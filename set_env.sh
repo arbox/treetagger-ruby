5d13644626c43d78996ce3734f5e312ebd8d045e
Andrei Beliankou <arbox@yandex.ru>
2012-08-25 20:39:18 +0200

    Altered the paths to tt modules.

--- set_env.sh
+++ set_env.sh
@@ -1,6 +1,8 @@
-#!/usr/bin/env sh
+# Source this file to set the environment for development.
+# $ source ./set_env.sh
 
 # Export env variables.
-export TREETAGGER_BINARY='/opt/TreeTagger/bin/tree-tagger'
-export TREETAGGER_MODEL='/opt/TreeTagger/lib/german.par'
-export TREETAGGER_LEXICON='/opt/TreeTagger/lib/german-lexicon.txt'
+export TREETAGGER_HOME='/home/arbox/work_space/treetagger-ruby/TreeTagger'
+export TREETAGGER_BINARY="$TREETAGGER_HOME/bin/tree-tagger"
+export TREETAGGER_MODEL="$TREETAGGER_HOME/lib/german.par"
+export TREETAGGER_LEXICON="$TREETAGGER_HOME/lib/german-lexicon.txt"
