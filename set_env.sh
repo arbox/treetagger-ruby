45f57e489be7fe3cbc4cb4b85ac79917749df2fb
Andrei Beliankou <arbox@yandex.ru>
2012-08-25 20:42:16 +0200

    Changed the path to the german model.

--- set_env.sh
+++ set_env.sh
@@ -4,5 +4,5 @@
 # Export env variables.
 export TREETAGGER_HOME='/home/arbox/work_space/treetagger-ruby/TreeTagger'
 export TREETAGGER_BINARY="$TREETAGGER_HOME/bin/tree-tagger"
-export TREETAGGER_MODEL="$TREETAGGER_HOME/lib/german.par"
+export TREETAGGER_MODEL="$TREETAGGER_HOME/lib/german-par-linux-3.2-utf8.par"
 export TREETAGGER_LEXICON="$TREETAGGER_HOME/lib/german-lexicon.txt"
